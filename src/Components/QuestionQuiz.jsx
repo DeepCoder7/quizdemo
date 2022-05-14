1c5a900def0a995832969b43164bfa6924f2854e
Pradeep <pradeepgupta36949@gmail.com>
2022-05-14 20:25:56 +0530

    make it better

--- src/Components/QuestionQuiz.jsx
+++ src/Components/QuestionQuiz.jsx
@@ -3,14 +3,14 @@ import './QuestionQuiz.css'
 
 const QuestionQuiz = (props) => {
     const { question, quizoptns } = props.quiz;
-    
+
     return (
         <div className='quizSection'>
             <div className="questionSection">
                 <b>{question}</b>
             </div>
             {quizoptns.map((quizOpt, index) => {
-                return <button key={index} onClick={() => { props.handleAns(quizOpt.isAnswer, index) }} className="answerOpt" disabled={quizOpt.clicked || props.quiz.answered} style={{background: quizOpt.clicked?`${quizOpt.color}`:'none'}} ><b>{quizOpt.answer}</b></button>
+                return <button key={index} onClick={() => { props.handleAns(quizOpt.isAnswer, index) }} className="answerOpt" disabled={quizOpt.clicked || props.quiz.answered} style={{ background: quizOpt.clicked ? `${quizOpt.color}` : 'none' }} ><b>{quizOpt.answer}</b></button>
             })}
         </div>
     )
